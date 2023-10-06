9ac18f2bcff0272ca75ce40ab4189fe77e5901a1
Utkarshkapil <kapilutk063@gmail.com>
2023-10-06 14:54:39 +0530

    Update getHelpDocUrl.ts
    tslint build error

--- apps/builder/src/features/graph/helpers/getHelpDocUrl.ts
+++ apps/builder/src/features/graph/helpers/getHelpDocUrl.ts
@@ -65,5 +65,7 @@ export const getHelpDocUrl = (blockType: BlockWithOptions['type']): string => {
       return 'https://docs.typebot.io/editor/blocks/integrations/pixel'
     case IntegrationBlockType.ZEMANTIC_AI:
       return 'https://docs.typebot.io/editor/blocks/integrations/zemantic-ai'
+    default:
+      return ""   
   }
 }
